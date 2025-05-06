6faa932e5c9c746dceb7a0da352eae2bce111ed7
jariperttunen <jari.perttunen@icloud.com>
2025-05-06 19:59:02 +0300

    Main program identifies coniferous and broadleaved tree from XML file and for broadleand the leaf geometery. Kite leaf is supported. Generate VTK/VTPC file.

--- lignumvtk.cc
+++ lignumvtk.cc
@@ -3,14 +3,20 @@
 #include <Lignum.h>
 #include <XMLTree.h>
 #include <LignumVTK.h>
+#include <LignumVTKXML.h>
 #include <LignumVTKTree.h>
 using namespace lignumvtk;
 
 int main(int argc,char* argv[])
 {
-  XMLDomTreeReader<VTKHwSegment,VTKBud,Kite> tree_reader;
+  
+  XMLDomTreeReader<VTKHwSegment,VTKBud,Kite> tree_hwreader;
+  XMLDomTreeReader<VTKCfSegment,VTKCfBud> tree_cfreader;
+  LignumVTKXML vtk_xml;
   LignumToVTK lignumvtk;
   Tree<VTKHwSegment,VTKBud> lignum_tree(Point(0,0,0), PositionVector(0,0,1.0));
+  //Tree<VTKCfSegment,VTKCfBud> lignum_cftree(Point(0,0,0), PositionVector(0,0,1.0));
+  Tree<VTKCfSegment,VTKCfBud> lignum_cftree;
   if (argc < 3 ){
     cout << "Usage: lignumtree.xml outputfile.vtpc [render]" << endl;
     cout << "Optional render -> Open and use VTK render window" << endl;
@@ -22,8 +28,29 @@ int main(int argc,char* argv[])
   }
   string tree_file = argv[1];
   string vtk_file = argv[2];
+  vtk_xml.setFileName(argv[1]);
+  vtk_xml.parse();
+  const string tree_type = vtk_xml.treeType();
+  const string leaf_type = vtk_xml.leafType();
+  if (tree_type == "Cf"){
+    cout << "Coniferous tree" <<endl;
+    cout << "Reading tree: " <<  tree_file <<endl;
+    tree_cfreader.readXMLToTree(lignum_cftree, tree_file);
+    cout << "Create conifer VTK data sets" <<endl;
+    lignumvtk.createConiferTreeVTKDataSets(lignum_cftree,render);
+    cout << "Write file" <<endl;
+    lignumvtk.writePartitionedDataSetCollection(vtk_file);
+    return EXIT_SUCCESS;
+  }
+  if (leaf_type != "Kite"){
+    cout << "Leaf type " << leaf_type << " not yeat supported" <<endl;
+    return EXIT_FAILURE;
+  }
+  cout << "Creating VTK file" <<endl;
+  cout << "Tree type: " << tree_type << " Leaf type: " << leaf_type <<endl;
+  
   cout << "Reading tree: " << tree_file <<endl; 
-  tree_reader.readXMLToTree(lignum_tree, tree_file);
+  tree_hwreader.readXMLToTree(lignum_tree, tree_file);
   lignumvtk.createBroadLeafTreeVTKDataSets(lignum_tree,render);
   lignumvtk.writePartitionedDataSetCollection(vtk_file);
   if (!render){
